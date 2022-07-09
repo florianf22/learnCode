b381990f7547738b3e82222f90dbed0dc85a3242
florianf22 <g.fasieshvili@gmail.com>
2022-07-10 00:03:51 +0400

    added more code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,4 +17,16 @@ export default NextAuth({
       return token;
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    // A secret to use for key generation. Defaults to the top-level `secret`.
+    secret: process.env.JWT_SECRET,
+    // The maximum age of the NextAuth.js issued JWT in seconds.
+    // Defaults to `session.maxAge`.
+    maxAge: 60 * 60 * 24 * 30,
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behavior.
+    async encode({ secret, token, maxAge }) {},
+    async decode({ secret, token }) {},
+  },
 });
