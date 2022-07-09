942add6fab59a7c9aad5c530c3d9e8e37c4962aa
florianf22 <g.fasieshvili@gmail.com>
2022-07-09 22:59:27 +0400

    added more code

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -1,72 +1,66 @@
-// import * as React from 'react';
-// import Head from 'next/head';
-// import { useRouter } from 'next/router';
-// import { formatAsPath } from '../../lib';
-// import { fetchCourse, fetchCourses } from '../../api/backend-helpers';
-// import Error from 'next/error';
-// import Container from '../../components/Container';
-// import CourseComponent from '../../components/Course';
+import * as React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { formatAsPath } from '../../lib';
+import { fetchCourse, fetchCourses } from '../../api/backend-helpers';
+import Error from 'next/error';
+import Container from '../../components/Container';
+import CourseComponent from '../../components/Course';
 
-// export default function Course({ data }) {
-//   const router = useRouter();
+export default function Course({ data }) {
+  const router = useRouter();
 
-//   const id = React.useMemo(() => formatAsPath(router.asPath), [router.asPath]);
+  const id = React.useMemo(() => formatAsPath(router.asPath), [router.asPath]);
 
-//   if (!data?.course) {
-//     return <Error title="Course could not be found" />;
-//   }
+  if (!data?.course) {
+    return <Error title="Course could not be found" />;
+  }
 
-//   return (
-//     <div>
-//       <Head>
-//         <title>{data.course.title}</title>
-//         <meta name="description" content="Find the perfect course for you!" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
+  return (
+    <div>
+      <Head>
+        <title>{data.course.title}</title>
+        <meta name="description" content="Find the perfect course for you!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-//       <Container>
-//         <CourseComponent course={data.course} />
-//       </Container>
-//     </div>
-//   );
-// }
+      <Container>
+        <CourseComponent course={data.course} />
+      </Container>
+    </div>
+  );
+}
 
-// export const getStaticProps = async context => {
-//   const id = context.params?.id;
+export const getStaticProps = async context => {
+  const id = context.params?.id;
 
-//   const course = await fetchCourse(id);
+  const course = await fetchCourse(id);
 
-//   if (!course) {
-//     return {
-//       props: {
-//         data: null,
-//       },
-//     };
-//   }
+  if (!course) {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
 
-//   return {
-//     props: {
-//       data: course,
-//     },
-//     revalidate: 20,
-//   };
-// };
-
-// export const getStaticPaths = async () => {
-//   const data = await fetchCourses();
-
-//   const paths = data.map(course => ({
-//     params: { id: course.published_title },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: true, // false or 'blocking'
-//   };
-// };
-
-const Course = ({ course }) => {
-  return <div></div>;
+  return {
+    props: {
+      data: course,
+    },
+    revalidate: 20,
+  };
 };
 
-export default Course;
+export const getStaticPaths = async () => {
+  const data = await fetchCourses();
+
+  const paths = data.map(course => ({
+    params: { id: course.published_title },
+  }));
+
+  return {
+    paths,
+    fallback: true, // false or 'blocking'
+  };
+};
