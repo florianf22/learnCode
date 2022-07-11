5d5a204d6c37ec8f02158d9c5a791c9058fcac58
florianf22 <g.fasieshvili@gmail.com>
2022-07-12 01:30:56 +0400

    youtube

--- app/youtube.js
+++ app/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBL3GElmvYt7PuF57jkFGsxNidnBKFCtVo';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResult: 20,
     type: 'video',
-    key: process.env.YOUTUBE_API_KEY,
+    key: 'AIzaSyC8RYd6X_zQuSQk4nbVdtgiEIcblX0MlYo',
   },
 });
